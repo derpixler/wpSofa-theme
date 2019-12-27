f80a145527b25da15650de038f725473e6daebe8
Rene Reimann <rene@die-pixler.de>
2019-12-27 08:56:46 +0100

    install localstorage lip

--- template-parts/player/assets/webpack.imports.js
+++ template-parts/player/assets/webpack.imports.js
@@ -2,8 +2,9 @@ let player = document.querySelector(".podcastPlayer");
 
 import(/* webpackChunkName: "player-scss" */'./scss/player.scss');
 
-window.loadIsInView(player, () => {
+window.observeElements(player, () => {
 	import(/* webpackChunkName: "player-js" */ "./js/player.js").then(player => {
 		import(/* webpackChunkName: "player-js" */ "./js/controls.js").then(controls => {});
+			player.default();
 	});
 });
